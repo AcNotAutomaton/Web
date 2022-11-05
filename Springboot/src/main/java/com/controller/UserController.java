9cd1177cc0131cd7af88f311dd5173ddf89bb6b9
AcNotAutomaton <550578884@qq.com>
2022-11-05 16:43:16 +0800

    Clean up the code

--- Springboot/src/main/java/com/controller/UserController.java
+++ Springboot/src/main/java/com/controller/UserController.java
@@ -5,10 +5,7 @@ import com.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class UserController {
