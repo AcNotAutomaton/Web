a6d84f4ee0548afcf0668c6ad39c82aa38471c9f
AcNotAutomaton <550578884@qq.com>
2022-11-24 16:46:28 +0800

    element-plus完成图书管理界面

--- Vue/src/views/systemManage/BookManage.vue
+++ Vue/src/views/systemManage/BookManage.vue
@@ -1,65 +1,82 @@
 <template>
-    <div class="bookInfo" style="height:110px">
-        <div>
-            <label>书名:</label>
-            <input type="text" v-model="newBook.bookName" />
-        </div>
-        <div>
-            <label>图片:</label>
-            <input type="text" v-model="newBook.bookUrl" />
-        </div>
-        <div>
-            <label>价格:</label>
-            <input type="text" v-model="newBook.price" />
-        </div>
-        <div>
-            <label>数量:</label>
-            <input type="text" v-model="newBook.count" />
-        </div>
-        <div>
-            <label>作者:</label>
-            <input type="text" v-model="newBook.author" />
-        </div>
-        <div>
-            <label>类型:</label>
-            <select v-model="newBook.type" style="width:175px;height:27px">
-                <option v-for="(item, index) in types" :key="index" :value="item.tname">{{ item.title }}</option>
-            </select>
-        </div>
-    </div>
-    <button v-on:click="addOrEditBook()">确定</button>&nbsp;&nbsp;&nbsp;<button v-on:click="reset()">重置</button>
+    <el-form :inline="true" :model="newBook" label-width="120px">
+        <el-row>
+            <el-col :span="7">
+                <el-form-item label="书名：">
+                    <el-input v-model="newBook.bookName" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="7">
+                <el-form-item label="图片：">
+                    <el-input v-model="newBook.bookUrl" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="7">
+                <el-form-item label="价格：">
+                    <el-input v-model="newBook.price" />
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="7">
+                <el-form-item label="数量：">
+                    <el-input v-model="newBook.count" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="7">
+                <el-form-item label="作者：">
+                    <el-input v-model="newBook.author" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="7">
+                <el-form-item label="类型：">
+                    <el-select v-model="newBook.type" placeholder="请选择图书类型">
+                        <el-option v-for="(item, index) in types" :key="index" :value="item.tname"
+                            :label="item.title" />
+                    </el-select>
+                </el-form-item>
+            </el-col>
+        </el-row>
+    </el-form>
+    <el-button type="primary" v-on:click="addOrEditBook()">确定</el-button>&nbsp;&nbsp;&nbsp;<el-button type="info"
+        v-on:click="reset()">重置</el-button>
     <br>
     <hr>
     <br />
-    书名：<input v-model="searchContent">&nbsp;<button v-on:click="search()">搜索
-    </button>&nbsp;<button v-on:click="cancelSearch()">取消搜索</button>
+    <el-row justify="center">
+        <el-form-item label="关键字：">
+            <el-input v-model="searchContent" />
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" :icon="Search" v-on:click="search()">搜索</el-button>
+            <el-button type="info" v-on:click="cancelSearch()">取消搜索</el-button>
+        </el-form-item>
+    </el-row>
     <br />
     <br />
-    <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>图片</th>
-                <th>书名</th>
-                <th>作者</th>
-                <th>价格</th>
-                <th>库存</th>
-                <th>类型</th>
-                <th>操作</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item, index) in books" :key="index">
-                <td><img style="width:40px;height:50px" :src="item.bookUrl"></td>
-                <td>{{ item.bookName }}</td>
-                <td>{{ item.author }}</td>
-                <td>{{ item.price }}</td>
-                <td>{{ item.count }}</td>
-                <td>{{ item.type }}</td>
-                <td><button v-on:click="deleteBook(item)">删除</button><button v-on:click="editBook(item)">编辑</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <el-table :data="books" style="width: 100%">
+        <el-table-column prop="bookUrl" label="图片" align="center">
+            <template #default="scope">
+                <el-image style="width: 40px; height: 50px" :src="scope.row.bookUrl" class="head_pic" />
+            </template>
+        </el-table-column>
+        <el-table-column prop=bookName label="书名" align="center">
+        </el-table-column>
+        <el-table-column prop=author align="center" label="作者">
+        </el-table-column>
+        <el-table-column prop=price align="center" label="价格">
+        </el-table-column>
+        <el-table-column prop=count align="center" label="库存">
+        </el-table-column>
+        <el-table-column prop=type align="center" label="类型">
+        </el-table-column>
+        <el-table-column align="center" label="操作">
+            <template #default="scope">
+                <el-button type="primary" icon="delete" v-on:click="deleteBook(scope.row)"></el-button>
+                <el-button type="danger" icon="edit" v-on:click="editBook(scope.row)"></el-button>
+            </template>
+        </el-table-column>
+    </el-table>
     <el-pagination :page-size="pageSize" :pager-count="pageCount" layout="prev, pager, next"
         v-model:current-page="currentPage" @current-change="showCurrentBookPage()" :total="pageTotal" />
     <br /> <br /> <br /> <br />
@@ -73,8 +90,6 @@
 </style>
 <script>
 import { reactive, toRefs, onMounted } from "vue";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
 import axios from "axios";
 export default {
     props: {
@@ -107,8 +122,6 @@ export default {
             types: [],
             books: [],
             searchContent: "",
-            searchBook: [],
-            oldBook: [],
             flag: "0",//0 代表添加状态 1 代表编辑状态
             editIndex: 0,//如果是编辑状态，被编辑的书籍的 index
             pageSize: 5,
@@ -138,6 +151,9 @@ export default {
                         .then((response) => {
                             alert("添加成功！");
                             data.showCurrentBookPage()
+                            data.newBook = {
+                                bookName: "", bookUrl: "", price: 0, count: 0, author: "", type: ""
+                            };
                         }).catch((error) => console.log(error)); // 请求失败处理
                 }
             },
@@ -153,24 +169,16 @@ export default {
                 data.newBook = item;
             },
             search: () => {
-                data.searchBook = data.books.filter((item) => {
-                    var index = item.bookName.indexOf(data.searchContent);
-                    if (index >= 0) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
-                data.oldBook = data.books;
-                data.books = data.searchBook;
+                data.showCurrentBookPage();
             },
             cancelSearch: () => {
-                data.books = data.oldBook;
+                data.searchContent = "";
+                data.showCurrentBookPage();
             },
             showCurrentBookPage: () => {
                 axios
                     .get("books?_page=" + data.currentPage + "&_limit=" + data.pageSize + "&_order=asc"
-                    )
+                        + "&q=" + data.searchContent)
                     .then((response) => {
                         debugger;
                         data.books = response.data;
