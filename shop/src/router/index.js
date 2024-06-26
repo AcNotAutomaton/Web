c2282c45ec6da9229582cf297a525cdee4880f80
Wa_Automata <550578884@qq.com>
2024-06-26 20:02:56 +0800

    modify shop router

--- shop/src/router/index.js
+++ shop/src/router/index.js
@@ -40,7 +40,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
